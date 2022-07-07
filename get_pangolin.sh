a3c2e3672315b47fa026e323c980bffcb2f98e48
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2022-07-07 16:32:27 -0500

    added fasta processing

---
+++ get_pangolin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Se encarga de procesar con pangolin
+#https://stackoverflow.com/questions/34534513/calling-conda-source-activate-from-bash-script
+eval "$(conda shell.bash hook)"
+conda activate GISAID
+for varname in $(ls fasta)
+do
+    idname=${varname%.*}
+    pangolin --threads 4 --outfile "fasta_processed/$idname.csv" --force fasta/$varname
+done
+conda deactivate
+
