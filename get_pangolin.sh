81b5e670e51c94734453f986e7c49ddfb2e0ee60
RodZepeda <rzepeda17@gmail.com>
2022-12-21 13:50:14 -0600

    new system

--- get_pangolin.sh
+++ get_pangolin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Se encarga de procesar con pangolin
 #https://stackoverflow.com/questions/34534513/calling-conda-source-activate-from-bash-script
-eval "$(/home/rodrigo/miniconda3/bin/conda shell.bash hook)"
+eval "$(/home/rod/miniconda3/bin/conda shell.bash hook)"
 conda activate pangolin
 for varname in $(ls fasta)
 do
