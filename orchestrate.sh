cc0b7bfc5fc32399641a5f6190265792f52556dc
Rodrigo Zepeda-Tello <rzepeda17@gmail.com>
2022-12-22 16:47:32 -0600

    Actualización 2022-12-22

--- orchestrate.sh
+++ orchestrate.sh
@@ -4,6 +4,8 @@
 #Author: Rodrigo Zepeda
 #Contact: rzepeda17[at]gmail.com
 #----------------------------------------
+#FROM https://stackoverflow.com/questions/55966634/unable-to-run-git-commands-with-crontab
+eval `ssh-agent -s` && ssh-add ~/.ssh/github && ssh-add -l
 cd /home/rod/VariantesCovid
 date=$(date '+%Y-%m-%d')
 /home/rod/miniconda3/envs/GISAID/bin/python3 /home/rod/VariantesCovid/download_gisaid.py
