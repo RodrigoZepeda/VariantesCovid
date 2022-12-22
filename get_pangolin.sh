76c3442d935f36efe8cd5b2aa505f3a50801424f
Rodrigo Zepeda-Tello <rzepeda17@gmail.com>
2022-12-22 16:47:32 -0600

    Actualización 2022-12-22

--- get_pangolin.sh
+++ get_pangolin.sh
@@ -6,7 +6,7 @@ conda activate pangolin
 for varname in $(ls fasta)
 do
     idname=${varname%.*}
-    /home/rod/miniconda3/bin/pangolin --threads 4 --outfile "fasta_processed/$idname.csv" fasta/$varname
+    pangolin --threads 4 --outfile "fasta_processed/$idname.csv" fasta/$varname
 done
 conda deactivate
 
